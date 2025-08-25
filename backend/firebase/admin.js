17d46b4ac478ad7f97b29387bc758c4bb68d527b
@Phantom101 <valdoramakuela@gmail.com>
2025-08-25 13:37:08 -0400

    Update admin.js

--- backend/firebase/admin.js
+++ backend/firebase/admin.js
@@ -1,9 +1,9 @@
-// firebase/admin.js
-const admin = require('firebase-admin');
-const serviceAccount = require('./serviceAccountKey.json'); //from Firebase Console
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert(serviceAccount)
 });
 
 module.exports = admin;
